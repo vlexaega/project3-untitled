a3d829915ae5f65a2302e7729bc9c54b33b27dcd
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-28 10:42:50 -0400

    adds code for User.js model

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        match: [/.+@.+\..+/, 'Must be an email address'],
+    },
+    password: {
+        type: String, 
+        required: true, 
+        minlength: 8,
+    },
+});
+
+//set up a pre-save middleware to create a password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')){
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+//compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
