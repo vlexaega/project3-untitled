c34da424e32c369763bb7f2a9262f74cd883ef7e
LeAnne B <branchleannelmb@gmail.com>
2023-07-28 11:14:38 -0400

    fixes the pages folder so it is not within components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-// import Nav from './components/Nav';
+// import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
   uri: "/graphql",
