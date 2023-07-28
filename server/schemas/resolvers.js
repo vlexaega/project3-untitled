27de31e97e0d1ca3daa66457ab8b6611f39da33f
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-28 10:05:23 -0500

    edits resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     Query: {
         user: async (parent, args, context) => {
             if (context.user) {
-                const user = await User.findById(context.user._id).
+                const user = await User.findById(context.user._id)
                 return user
             }
         }
