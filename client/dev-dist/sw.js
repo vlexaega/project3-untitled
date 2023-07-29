29e62058b89ad36a070c8278f1e100bee6bdf544
LeAnne B <branchleannelmb@gmail.com>
2023-07-29 10:17:12 -0400

    styles signup page

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nj55q84p3c"
+    "revision": "0.isdhtf885p"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
