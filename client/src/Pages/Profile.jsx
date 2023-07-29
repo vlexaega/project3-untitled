070145cace8600575a01fb61a9222515185aa468
Calvin Roberts <you@example.com>
2023-07-29 13:39:58 -0600

    added custom logo colors to tailwind config

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 function Profile() {
   return (
@@ -46,13 +47,6 @@ function Profile() {
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                        type="button"
-                        style={{ transition: "all .15s ease" }}
-                      >
-                        Connect
-                      </button>
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
@@ -61,7 +55,7 @@ function Profile() {
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           22
                         </span>
-                        <span className="text-sm text-gray-500">Friends</span>
+                        <span className="text-sm text-gray-500">Uploads</span>
                       </div>
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
@@ -79,8 +73,8 @@ function Profile() {
                   </div>
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                    Jenna Stones
+                  <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2">
+                    
                   </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
