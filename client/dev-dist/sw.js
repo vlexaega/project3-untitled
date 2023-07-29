22a5c3710bb88362df0e86ac457edb5590b7b25c
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-28 21:29:38 -0500

    adds link and script to index.html file

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fs114fpen5o"
+    "revision": "0.48cndtg0tag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
