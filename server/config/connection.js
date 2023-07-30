df7c68eb37fd6a7c882db0b177f1f17fec6f7f37
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-30 13:46:02 -0400

    updates database name to untitled-art-app and new user creation is persisting in the database

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/untitled-art-app');
 
 module.exports = mongoose.connection;
