12ddb6c2717d18e8b078f48c3eb748ea81421adb
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-30 15:41:39 -0400

    front end image upload works and file is converted to base64 code.

--- client/src/Pages/UploadArt.jsx
+++ client/src/Pages/UploadArt.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 
 function ImageUpload(){
 
+    const[image, setImage]=useState("");
+
     function convertToBase64(e) {
         console.log(e);
         var reader = new FileReader();
-        reader.readAsDataURL(e.target.file[0]);
+        reader.readAsDataURL(e.target.files[0]);
         reader.onload = () => {
             console.log(reader.result); //this returns the base64encoded string
+            setImage(reader.result);
         };
         reader.onerror = error => {
             console.log("Error: ", error);
@@ -16,12 +19,14 @@ function ImageUpload(){
     return (
         <div className="auth-wrapper" >
             <div className="auth-inner" style={{ width: "auto" }}>
-                Upload your artwork
+                Upload your artwork<br/>
                 <input
                 accept="image/*"
                 type="file"
                 onChange={convertToBase64}
                 />
+                {image=="" || image==null ? "" : <img width={100} height={100} src={image}/>}
+                
             </div>
         </div>
     )
