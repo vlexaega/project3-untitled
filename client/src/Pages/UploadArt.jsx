ee02317df9ef3234cb79011ba47eb4c8aaf53aec
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-30 15:50:59 -0500

    adds sales options check boxes

--- client/src/Pages/UploadArt.jsx
+++ client/src/Pages/UploadArt.jsx
@@ -286,14 +286,13 @@ const UploadArt = () => {
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Sale Options</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what else you want to hear about.
+          Would you like to sell your work as a digital download or physical artwork? Choose any, all, or none of the options below:
           </p>
 
           <div className="mt-10 space-y-10">
             <fieldset>
-              <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
               <div className="mt-6 space-y-6">
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
@@ -306,9 +305,9 @@ const UploadArt = () => {
                   </div>
                   <div className="text-sm leading-6">
                     <label htmlFor="comments" className="font-medium text-gray-900">
-                      Comments
+                      Digital Download
                     </label>
-                    <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                    <p className="text-gray-500">Check this box to sell your work as a digital download</p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
@@ -322,9 +321,9 @@ const UploadArt = () => {
                   </div>
                   <div className="text-sm leading-6">
                     <label htmlFor="candidates" className="font-medium text-gray-900">
-                      Candidates
+                      Physical Artwork
                     </label>
-                    <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
+                    <p className="text-gray-500">Check this box to sell your work as a physical product</p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
@@ -335,12 +334,6 @@ const UploadArt = () => {
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
-                  </div>
-                  <div className="text-sm leading-6">
-                    <label htmlFor="offers" className="font-medium text-gray-900">
-                      Offers
-                    </label>
-                    <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
                   </div>
                 </div>
               </div>
