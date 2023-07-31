482ec4a38f3739e07a225e7dcb1c04a7d22440dd
Calvin Roberts <you@example.com>
2023-07-31 15:17:03 -0600

    added placeholder divs for artists and added styling

--- client/src/Pages/Artists.jsx
+++ client/src/Pages/Artists.jsx
@@ -7,6 +7,18 @@ function Artists() {
     <>
       <main>
         <Navbar />
+        <div className="grid grid-cols-4 gap-4 hover:grid-cols-6 box-border bg-gradient-to-r from-logo-pink via-red-400 to-blue-300">
+          <div>01</div>
+          <div>02</div>
+          <div>03</div>
+          <div>04</div>
+          <div>05</div>
+          <div>06</div>
+          <div>07</div>
+          <div>08</div>
+          <div>09</div>
+          <div>10</div>
+        </div>
       </main>
     </>
   );
