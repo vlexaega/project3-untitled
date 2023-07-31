78febeec175fc7e59457941e1d594017636b5e6c
Calvin Roberts <you@example.com>
2023-07-31 10:44:46 -0600

    created artists.jsx page and linked it  to main.jsx

---
+++ client/src/Pages/Artists.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Artists() {
+  return (
+    <>
+      <main>
+        <Navbar />
+      </main>
+    </>
+  );
+}
+
+export default Artists;
