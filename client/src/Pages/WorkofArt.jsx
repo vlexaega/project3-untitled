611eb04273d8a53c9fb2abcfb4063cc1d36346e0
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 13:10:23 -0400

    removes extra images and modal feature on workofart page as it is not needed

--- client/src/Pages/WorkofArt.jsx
+++ client/src/Pages/WorkofArt.jsx
@@ -51,33 +51,6 @@ const WorkofArt = () => {
   //   const fixedPrice = 99.99;
   const fixedPrice = price.toFixed(2);
 
-  const totalPrice = fixedPrice * qty;
-  const totalPriceFixed = totalPrice.toFixed(2);
-  //   const totalPriceFixed = 89.99;
-
-  const decrease = () => {
-    if (qty === 0) {
-      return;
-    }
-    {
-      setQty((prev) => prev - 1);
-    }
-  };
-
-  const toggleModal = () => {
-    setModal((prev) => !prev);
-  };
-
-  const goBack = () => {
-    value === 0 ? setValue(0) : setValue((prev) => prev - 1);
-  };
-
-  const goForward = () => {
-    value === products.length - 1
-      ? setValue(products.length - 1)
-      : setValue((prev) => prev + 1);
-  };
-
   return (
     <div className="container">
       <Navbar />
@@ -87,96 +60,10 @@ const WorkofArt = () => {
           <div className="image md:basis-1/2 md:flex md:flex-col md:justify-between">
             <div className="hidden md:block large-image">
               <img
-                onClick={toggleModal}
-                className="object-cover cursor-pointer rounded-xl w-[400px] h-[400px]"
+                className="object-cover rounded-xl w-[400px] h-[400px]"
                 src={largeImage}
                 alt="snekers-photo"
               />
-            </div>
-            <div className="md:hidden large-image">
-              <img
-                onClick={goBack}
-                className="bg-white rounded-full p-4 absolute top-[15%] left-6 cursor-pointer"
-                src={prevIcon}
-                alt="go-back"
-              />
-              <img
-                className="w-[100%] h-[300px] object-cover"
-                src={largeImage}
-                alt="snekers-photo"
-              />
-              <img
-                onClick={goForward}
-                className="bg-white rounded-full p-4 absolute top-[15%] left-[82%] cursor-pointer"
-                src={nextIcon}
-                alt="go-forward"
-              />
-            </div>
-            <div className="small-images hidden md:flex mt-7 justify-between w-[400px]">
-              {data.map((img, idx) => {
-                return (
-                  <div key={img.id} className="single-image">
-                    <img
-                      onClick={() => setValue(idx)}
-                      className="w-[80px] cursor-pointer rounded-xl transition-all hover:opacity-25 hover:border-[3px] border-orange"
-                      src={img.smallImg}
-                      alt="product-photo"
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-
-          <div
-            className={`${
-              modal ? "hidden" : "hidden md:block"
-            } absolute -top-[20%] right-0 -bottom-[20%] left-0 bg-lightBlack`}
-          >
-            <div
-              className={
-                "basis-1/2 flex flex-col justify-between absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
-              }
-            >
-              <div className="large-image">
-                <img
-                  className="w-[400px] h-[400px] rounded-xl cursor-pointer"
-                  src={largeImage}
-                  alt="snekers-photo"
-                />
-                <img
-                  onClick={toggleModal}
-                  className="w-[20px] h-[20px] absolute -top-8 left-[95%] transition-all cursor-pointer hover:scale-150"
-                  src={closeIcon}
-                  alt="close-icon"
-                />
-                <img
-                  onClick={goBack}
-                  className="bg-white p-4 rounded-full absolute top-[36%] -translate-x-[20px] cursor-pointer transition-all hover:scale-150"
-                  src={prevIcon}
-                  alt="previous"
-                />
-                <img
-                  onClick={goForward}
-                  className="bg-white p-4 rounded-full absolute top-[36%] left-[95%] cursor-pointer transition-all hover:scale-150"
-                  src={nextIcon}
-                  alt="next"
-                />
-              </div>
-              <div className="small-images flex mt-7 justify-around w-[400px]">
-                {data.map((img, idx) => {
-                  return (
-                    <div key={img.id} className="single-image">
-                      <img
-                        onClick={() => setValue(idx)}
-                        className="w-[60px] cursor-pointer rounded-xl transition-all hover:border-[3px] border-orange"
-                        src={img.smallImg}
-                        alt="product-photo"
-                      />
-                    </div>
-                  );
-                })}
-              </div>
             </div>
           </div>
 
