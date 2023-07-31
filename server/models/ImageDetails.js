6b4ad4699088775ccda8b4c93c8a72fe138da81f
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-07-31 10:28:05 -0500

    updates ImageDetails.jsx in the models folder

--- server/models/ImageDetails.js
+++ server/models/ImageDetails.js
@@ -6,6 +6,47 @@ const ImageDetailsSchema = new Schema({
     image: {
         type: String,
         required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    isOriginal: {
+        type: Boolean,
+        required: true,
+    },
+    medium: {
+        type: String,
+        required: true,
+    },
+    canCritique: {
+        type: Boolean,
+        required: false,
+    },
+    canDownload: {
+        type: Boolean,
+        required: false,
+    },
+    downloadPrice: {
+        type: Number,
+        required: false,
+    },
+    canPurchase: {
+        type: Boolean,
+        required: false,
+    },
+    purchasePrice: {
+        type: Number,
+        required: false,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
     }
 }
     
