8e067f37c9ff976a186770768aeff542071ae004
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 10:46:41 -0400

    adds in tutorial credit in readme

--- client/src/Pages/WorkofArt.jsx
+++ client/src/Pages/WorkofArt.jsx
@@ -1,4 +1,10 @@
 import Navbar from "../components/Navbar";
+import React, { useState } from "react";
+import cartIcon from "../assets/icon-cart-white.svg";
+import closeIcon from "../assets/icon-close-white.svg";
+import prevIcon from "../assets/icon-previous.svg";
+import nextIcon from "../assets/icon-next.svg";
+import { data } from "../constants/images";
 
 const WorkofArt = () => {
   return (
