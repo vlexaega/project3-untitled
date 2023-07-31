e5e44244ed70c30bf3808f329d29d967cd5f6248
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 13:59:08 -0400

    fixes navbar so that it goes all the way across the page on workofart

--- client/src/Pages/WorkofArt.jsx
+++ client/src/Pages/WorkofArt.jsx
@@ -53,53 +53,55 @@ const WorkofArt = () => {
   const fixedPrice = price.toFixed(2);
 
   return (
-    <div className="container">
+    <div>
       <Navbar />
-      <h1 className="text-6xl text-center p-8">Work of Art</h1>
-      <main>
-        <div className="main-wrapper flex flex-col md:flex-row md:px-[200px] md:py-[100px] relative">
-          <div className="image md:basis-1/2 md:flex md:flex-col md:justify-between">
-            <div className="md:block large-image">
-              <img
-                className="object-cover mx-auto rounded-xl w-[400px] h-[400px]"
-                src={largeImage}
-                alt="snekers-photo"
-              />
-            </div>
-          </div>
-
-          <div className="description p-6 md:basis-1/2 md:py-[40px]">
-            <p className="text-orange text-[14px] tracking-widest uppercase font-[700] mb-6">
-              Artist Name
-            </p>
-            <h1 className="text-3xl md:text-4xl capitalize font-[700]">
-              Name of art piece
-            </h1>
-            <p className="hidden md:block text-darkGrayishBlue my-10 leading-7">
-              Description of piece goes here.
-            </p>
-            <p className="md:hidden text-darkGrayishBlue my-6 leading-7">
-              Description of piece goes here.
-            </p>
-
-            <div className="price flex items-center">
-              <span className="text-3xl font-[700] mr-4">${fixedPrice}</span>
-            </div>
-
-            <div className="buttons-container flex flex-col md:flex-row mt-8">
-              <button className="add-btn border-none bg-logo-pink rounded-lg text-black font-[700] py-6 px-6 md:text-[14px] transition-all btn-shadow hover:opacity-50">
+      <div className="container">
+        <h1 className="text-6xl text-center p-8">Work of Art</h1>
+        <main>
+          <div className="main-wrapper flex flex-col md:flex-row md:px-[200px] md:py-[100px] relative">
+            <div className="image md:basis-1/2 md:flex md:flex-col md:justify-between">
+              <div className="md:block large-image">
                 <img
-                  className="inline-block -translate-x-2 -translate-y-[2px] h-[15px]"
-                  src={cartIcon}
-                  alt="cart-icon"
+                  className="object-cover mx-auto rounded-xl w-[400px] h-[400px]"
+                  src={largeImage}
+                  alt="snekers-photo"
                 />
-                &nbsp;Add to cart
-              </button>
-              {/* <CommentForm /> */}
+              </div>
+            </div>
+
+            <div className="description p-6 md:basis-1/2 md:py-[40px]">
+              <p className="text-orange text-[14px] tracking-widest uppercase font-[700] mb-6">
+                Artist Name
+              </p>
+              <h1 className="text-3xl md:text-4xl capitalize font-[700]">
+                Name of art piece
+              </h1>
+              <p className="hidden md:block text-darkGrayishBlue my-10 leading-7">
+                Description of piece goes here.
+              </p>
+              <p className="md:hidden text-darkGrayishBlue my-6 leading-7">
+                Description of piece goes here.
+              </p>
+
+              <div className="price flex items-center">
+                <span className="text-3xl font-[700] mr-4">${fixedPrice}</span>
+              </div>
+
+              <div className="buttons-container flex flex-col md:flex-row mt-8">
+                <button className="add-btn border-none bg-logo-pink rounded-lg text-black font-[700] py-6 px-6 md:text-[14px] transition-all btn-shadow hover:opacity-50">
+                  <img
+                    className="inline-block -translate-x-2 -translate-y-[2px] h-[15px]"
+                    src={cartIcon}
+                    alt="cart-icon"
+                  />
+                  &nbsp;Add to cart
+                </button>
+                {/* <CommentForm /> */}
+              </div>
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 };
