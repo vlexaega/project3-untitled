c0211f0416344c48584c2ea31c55195366ea27a7
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 10:55:46 -0400

    adds in test images into assets folder for this workofart page

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -1,10 +1,3 @@
-import Navbar from "../components/Navbar";
-import React, { useState } from "react";
-import cartIcon from "../assets/icon-cart-white.svg";
-import closeIcon from "../assets/icon-close-white.svg";
-import prevIcon from "../assets/icon-previous.svg";
-import nextIcon from "../assets/icon-next.svg";
-
 import large1 from "../assets/image-product-1.jpg";
 import large2 from "../assets/image-product-2.jpg";
 import large3 from "../assets/image-product-3.jpg";
@@ -15,8 +8,8 @@ import small2 from "../assets/image-product-2-thumbnail.jpg";
 import small3 from "../assets/image-product-3-thumbnail.jpg";
 import small4 from "../assets/image-product-4-thumbnail.jpg";
 
-
-const data = [  {
+export const data = [
+  {
     id: 1,
     largeImg: large1,
     smallImg: small1,
@@ -35,15 +28,5 @@ const data = [  {
     id: 4,
     largeImg: large4,
     smallImg: small4,
-  },];
-
-const WorkofArt = ({ price, qty, setQty }) => {
-  return (
-    <div className="container">
-      <Navbar />
-      <h1 className="text-6xl text-center p-8">Work of Art</h1>
-    </div>
-  );
-};
-
-export default WorkofArt;
+  },
+];
