c39da0a46bca969d78b60a3e00a3b424fc4014ec
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 09:11:26 -0400

    adds in workofart page

---
+++ client/src/Pages/WorkofArt.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+const WorkofArt = () => {
+    return (
+      <div className="container">
+        <Navbar />
+        <h1 className="text-6xl text-center p-8">Work of Art</h1>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
