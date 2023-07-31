112bed8a15191366d0ca044d3166161ba34e211c
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-07-31 12:21:48 -0400

    additional commit for safekeeping

--- client/src/Pages/Upload.jsx
+++ client/src/Pages/Upload.jsx
@@ -54,56 +54,6 @@ function ImageUpload() {
       console.error("Error uploading image: ", error.message);
     }
   }
-//   return (
-//     <div className="auth-wrapper">
-//       <Navbar />
-    
-//       <h2>Add New Artwork</h2>
-//         <form>   
-//         <div className="mt-10 space-y-10">
-//             <fieldset>
-//               <legend className="text-sm font-semibold leading-6 text-gray-900">Declaration</legend>
-//               <div className="mt-6 space-y-6">
-//                 <div className="relative flex gap-x-3">
-//                   <div className="flex h-6 items-center">
-//                     <input
-//                       id="comments"
-//                       name="comments"
-//                       type="checkbox"
-//                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-//                     />
-//                   </div>
-//                   <div className="text-sm leading-6">
-//                     <label htmlFor="comments" className="font-medium text-gray-900">
-//                       I confirm that this is my own artwork and I have the right to distribute this work
-//                     </label>
-//                   </div>
-//                 </div>
-//               </div>
-//             </fieldset>
-//         </div>
-
-
-
-//       <div className="auth-inner" style={{ width: "auto" }}>
-//         Upload your artwork
-//         <br />
-//         <input accept="image/*" type="file" onChange={convertToBase64} />
-//         {image === "" || image === null ? (
-//           ""
-//         ) : (
-//           <img width={100} height={100} src={image} />
-//         )}
-//         <button onClick={handleUploadImage}>Upload</button>
-//       </div>
-      
-      
-       
-//     </form>
-//     </div>
-//   );
-// }
-
 
 return (
     <div>
@@ -157,17 +107,6 @@ return (
                   </div>
                   <p className="text-xs leading-5 text-gray-600">PNG or JPG up to 10MB</p>
                 </div>
-
-            {/* <div className="auth-inner" style={{ width: 'auto' }}>
-                Upload your artwork<br />
-                <input accept="image/*" type="file" onChange={convertToBase64} />
-                {image === '' || image === null ? (
-                  ''
-                ) : (
-                  <img width={100} height={100} src={image} />
-                )}
-                <button onClick={handleUploadImage}>Upload</button>
-              </div> */}
               </div>
             </div>
 
