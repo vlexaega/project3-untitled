bc29cc5ca25867058b5183b9a46b03e67100669c
University <university@AA687692-Mac.local>
2023-07-31 14:18:18 -0500

    removed the div for the button and path for the image that came with the tailwind template - the notification bell and user profile picture.

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -23,18 +23,9 @@ function Navbar() {
                         Menu open: "hidden", Menu closed: "block"
                     --> */}
               <svg
-                className="block h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
+              
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
+              
               </svg>
               {/* <!--
                         Icon when menu is open.
@@ -42,12 +33,7 @@ function Navbar() {
                         Menu open: "block", Menu closed: "hidden"
                     --> */}
               <svg
-                className="hidden h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
+                
               >
                 <path
                   strokeLinecap="round"
@@ -69,7 +55,7 @@ function Navbar() {
               <div className="flex space-x-4">
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 {/* if logged in is true */}
-                {!Auth.loggedIn() ? (
+                {Auth.loggedIn() ? (
                   <>
                     <Link
                       to="/home"
@@ -137,27 +123,8 @@ function Navbar() {
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-          
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                />
-              </svg>
-            </button>
+          <div>
+           
 
             {/* <!-- Profile dropdown --> */}
             <div className="relative ml-3">
@@ -172,7 +139,7 @@ function Navbar() {
 
                   <span className="sr-only">Open user menu</span>
                   <img
-                    className="h-8 w-8 rounded-full"
+                  
                     alt=""
                   />
                 </button>
