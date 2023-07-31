26912d8ce461fc600eb87e1082712dd1ce481d9d
LeAnne B <branchleannelmb@gmail.com>
2023-07-31 09:11:57 -0400

    removes extra closing div tag from work of art page

--- client/src/Pages/WorkofArt.jsx
+++ client/src/Pages/WorkofArt.jsx
@@ -1,14 +1,12 @@
 import Navbar from "../components/Navbar";
 
 const WorkofArt = () => {
-    return (
-      <div className="container">
-        <Navbar />
-        <h1 className="text-6xl text-center p-8">Work of Art</h1>
-        </div>
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <div className="container">
+      <Navbar />
+      <h1 className="text-6xl text-center p-8">Work of Art</h1>
+    </div>
+  );
+};
+
+export default Home;
