3ac501978a01f70b4f77ab403f9ea132c5b511ee
LeAnne B <branchleannelmb@gmail.com>
2023-08-01 15:08:09 -0400

    removes jumbotron from artbymedium page

--- client/src/pages/ArtbyMedium.jsx
+++ client/src/pages/ArtbyMedium.jsx
@@ -1,13 +1,10 @@
-import Jumbotron from "../components/Jumbotron";
 import Navbar from "../components/Navbar";
 
 const ArtByMedium = () => {
   return (
     <div>
       <Navbar />
-      <Jumbotron>
-        <h1 className="text-6xl text-center p-8">Art by Medium</h1>
-      </Jumbotron>
+      <h1 className="text-6xl text-center p-8">Art by Medium</h1>
     </div>
   );
 };
