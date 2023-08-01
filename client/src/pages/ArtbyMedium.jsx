cfe6e77094af666cf35703b62985dd306b323585
LeAnne B <branchleannelmb@gmail.com>
2023-08-01 17:12:43 -0400

    labels each medium box

--- client/src/pages/ArtbyMedium.jsx
+++ client/src/pages/ArtbyMedium.jsx
@@ -4,13 +4,7 @@ const ArtByMedium = () => {
   return (
     <div>
       <Navbar />
-      <h1 classNameName="text-6xl text-center p-8">Art by Medium</h1>
-      <p>Painting</p>
-      <p>Drawing</p>
-      <p>Mixed Media</p>
-      <p>Digital</p>
-      <p>Fabric</p>
-      <p>Photography</p>
+      <h1 className="text-6xl text-center p-8">Art by Medium</h1>
       <div className="container my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap -mx-1 lg:-mx-4">
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
@@ -29,7 +23,7 @@ const ArtByMedium = () => {
                     className="no-underline hover:underline text-black"
                     href="#"
                   >
-                    Article Title
+                    Paintings
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">11/1/19</p>
@@ -74,7 +68,52 @@ const ArtByMedium = () => {
                     className="no-underline hover:underline text-black"
                     href="#"
                   >
-                    Article Title
+                    Drawings
+                  </a>
+                </h1>
+                <p className="text-grey-darker text-sm">11/1/19</p>
+              </header>
+
+              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                <a
+                  className="flex items-center no-underline hover:underline text-black"
+                  href="#"
+                >
+                  <img
+                    alt="Placeholder"
+                    className="block rounded-full"
+                    src="https://picsum.photos/32/32/?random"
+                  />
+                  <p className="ml-2 text-sm">uhoh</p>
+                </a>
+                <a
+                  className="no-underline text-grey-darker hover:text-red-dark"
+                  href="#"
+                >
+                  <span className="hidden">Like</span>
+                  <i className="fa fa-heart"></i>
+                </a>
+              </footer>
+            </article>
+          </div>
+
+          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+            <article className="overflow-hidden rounded-lg shadow-lg">
+              <a href="#">
+                <img
+                  alt="Placeholder"
+                  className="block h-auto w-full"
+                  src="https://picsum.photos/600/400/?random"
+                />
+              </a>
+
+              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                <h1 className="text-lg">
+                  <a
+                    className="no-underline hover:underline text-black"
+                    href="#"
+                  >
+                    Mixed Media
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">11/1/19</p>
@@ -119,7 +158,7 @@ const ArtByMedium = () => {
                     className="no-underline hover:underline text-black"
                     href="#"
                   >
-                    Article Title
+                    Digital
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">11/1/19</p>
@@ -164,7 +203,7 @@ const ArtByMedium = () => {
                     className="no-underline hover:underline text-black"
                     href="#"
                   >
-                    Article Title
+                    Fabric
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">11/1/19</p>
@@ -209,52 +248,7 @@ const ArtByMedium = () => {
                     className="no-underline hover:underline text-black"
                     href="#"
                   >
-                    Article Title
-                  </a>
-                </h1>
-                <p className="text-grey-darker text-sm">11/1/19</p>
-              </header>
-
-              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a
-                  className="flex items-center no-underline hover:underline text-black"
-                  href="#"
-                >
-                  <img
-                    alt="Placeholder"
-                    className="block rounded-full"
-                    src="https://picsum.photos/32/32/?random"
-                  />
-                  <p className="ml-2 text-sm">Author Name</p>
-                </a>
-                <a
-                  className="no-underline text-grey-darker hover:text-red-dark"
-                  href="#"
-                >
-                  <span className="hidden">Like</span>
-                  <i className="fa fa-heart"></i>
-                </a>
-              </footer>
-            </article>
-          </div>
-
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
-
-              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                <h1 className="text-lg">
-                  <a
-                    className="no-underline hover:underline text-black"
-                    href="#"
-                  >
-                    Article Title
+                    Photography
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">11/1/19</p>
