c2ec5da62e77f84b76d697bac9f4215083a723f5
Calvin Roberts <you@example.com>
2023-08-01 14:16:53 -0600

    re-styled profile page and changed background

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import Logo from "../assets/LogoBackdrop.png";
+// import Logo from "../assets/LogoBackdrop.png";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -47,38 +48,36 @@ function Profile() {
     return <div>Error collecting data</div>;
   }
 
-  // const [userName, setUserName] = useState("");
-  // const [bio, setUserBio] = useState("");
-  // // retrieve username from local storage and set it
-  // useEffect(() => {
-  //   const storedUserName = localStorage.getItem("userName");
-  //   if (storedUserName) {
-  //     setUserName(storedUserName);
-  //   }
-  // }, []);
-  // // retrieve bio form localstorage and set it
-  // useEffect(() => {
-  //   const storedBio = localStorage.getItem("bio");
-  //   if (storedBio) {
-  //     setUserBio(storedBio);
-  //   }
-  // }, []);
   return (
     <>
       <main className="profile-page">
         <Navbar />
+
         <section className="relative block" style={{ height: "500px" }}>
           <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage: `url(${Logo})`,
-            }}
+            className="absolute top-0 w-full h-full bg-center bg-cover bg-gradient-to-r from-indigo-300 via-logo-pink to-red-200"
+            // style={{
+            //   backgroundImage: `url(${Logo})`,
+            // }}
           >
+            <div className="mb-2 text-logo-black shadow-xl shadow-indigo-500/50  text-5xl text-center p-8">
+              <h3 className="mb-2 mt-10  text-6xl">{userName}</h3>
+              <h3 className="mb-2 mt-10 text-2xl">{bio}</h3>
+              {/* <div className="mt-10 py-10 border-t flex flex-wrap justify-center border-logo-black text-center">
+                <div className="w-full lg:w-9/12 px-4">
+                  <p className="mb-4 text-lg leading-relaxed text-logo-black">
+                    
+                  </p>
+                </div>
+              </div> */}
+            </div>
+
             <span
               id="LogoPinkOverlay"
               className="w-full h-full absolute opacity-10 bg-logo-pink"
             ></span>
           </div>
+
           <div
             className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
             style={{ height: "70px" }}
@@ -92,14 +91,14 @@ function Profile() {
               x="0"
               y="0"
             >
-              <polygon
-                className="text-gray-300 fill-current"
+              {/* <polygon
+                className="text-center text-red-200 fill-current"
                 points="2560 0 2560 100 0 100"
-              ></polygon>
+              ></polygon> */}
             </svg>
           </div>
         </section>
-        <section className="relative py-16 bg-gray-300">
+        <section className="relative py-16 bg-gradient-to-r from-indigo-300 via-logo-pink to-red-200">
           <div className="container mx-auto px-4">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
               <div className="px-6">
@@ -110,24 +109,16 @@ function Profile() {
                   <div className="w-full lg:w-4/12 xl:w-1/6 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0"></div>
                   </div>
-                  <div className="">
-                    <div className="mb-2 text-logo-black mt-10">
-                      <h3 className="mb-2 mt-10">Hey there,</h3>
-                      {userName}
-                    </div>
-                  </div>
+
                   <div className="w-full flex justify-center text-center items-center">
                     <div className="flex justify-center py-4 lg:pt-4 pt-8 flex-wrap items-center text-center">
                       <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold text-center block uppercase tracking-wide text-gray-700">
-                          22
-                        </span>
-                        <span className="text-sm text-logo-black">Uploads</span>
                         <div className="flex flex-wrap justify-center">
                           <div className="w-full lg:w-9/12 px-4">
-                            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-                              Uploaded Images
+                            <h2 className="text-2xl font-semibold border-b border-indigo-800 text-logo-black mb-4">
+                              {userName}'s Art
                             </h2>
+                            <br></br>
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                               {userImages.map((image) => (
                                 <div
@@ -140,37 +131,28 @@ function Profile() {
                                     className="w-full h-40 object-cover"
                                   />
                                   <div className="p-4">
-                                    <h3 className="text-lg font-semibold text-gray-800">
+                                    <h3 className="text-lg font-semibold text-logo-black">
                                       {image.title}
                                     </h3>
-                                    <p className="text-gray-600">
+                                    <p className="text-gray-700">
                                       {image.description}
                                     </p>
                                   </div>
                                 </div>
                               ))}
                             </div>
+                            <div className="text-center mt-12">
+                              <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2"></h3>
+
+                              <div className="mb-2 text-logo-black mt-10">
+                                <button className="bg-logo-pink hover:bg-red-200 text-logo-black text-lg font-bold py-4 px-4 rounded">
+                                  <Link to="/upload">Upload Your Art!</Link>
+                                </button>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal text-gray-800 mb-2"></h3>
-
-                  <div className="mb-2 text-logo-black mt-10">
-                    <button className="bg-logo-pink hover:bg-red-200 text-logo-black font-bold py-2 px-4 rounded">
-                      <Link to="/upload">Upload Your Art!</Link>
-                    </button>
-                  </div>
-                </div>
-                <div className="mt-10 py-10 border-t border-gray-300 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-logo-black">
-                        {bio}
-                      </p>
                     </div>
                   </div>
                 </div>
