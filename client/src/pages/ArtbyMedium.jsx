82825ea895c3fe537d826e7322a7d2a240d22065
LeAnne B <branchleannelmb@gmail.com>
2023-08-01 15:18:16 -0400

    Adds in medium sections

--- client/src/pages/ArtbyMedium.jsx
+++ client/src/pages/ArtbyMedium.jsx
@@ -5,6 +5,12 @@ const ArtByMedium = () => {
     <div>
       <Navbar />
       <h1 className="text-6xl text-center p-8">Art by Medium</h1>
+      <p>Painting</p>
+                <p>Drawing</p>
+                <p>Mixed Media</p>
+                <p>Digital</p>
+                <p>Fabric</p>
+                <p>Photography</p>
     </div>
   );
 };
