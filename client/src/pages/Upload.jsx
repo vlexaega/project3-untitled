d2c618ec554bdbdd61a83c3b0e348321107c9083
LeAnne B <branchleannelmb@gmail.com>
2023-08-01 16:26:15 -0400

    does a bit of styling for upload button on upload page to match logo color

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -372,7 +372,7 @@ return (
         </button>
         <button 
         type="submit"
-        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="rounded-md bg-logo-pink px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         onClick={handleUploadImage}>
           Upload
         </button>
