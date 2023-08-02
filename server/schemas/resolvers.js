3e0eeaa77a73b4692d9b331a183fc443ba284448
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-08-02 13:00:46 -0400

    adds Queries of categories, products and product

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,27 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    categories: async () => {
+      return await Category.find();
+    },
+    products: async (parent, { category, name }) => {
+      const params = {};
+
+      if (category) {
+        params.category = category;
+      }
+
+      if (name) {
+        params.name = {
+          $regex: name
+        };
+      }
+
+      return await Product.find(params).populate('category');
+    },
+    product: async (parent, { _id }) => {
+      return await Product.findById(_id).populate('category');
+    },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id);
