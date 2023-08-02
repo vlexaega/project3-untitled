348417ec63270916a044a323a02cff690288a14b
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-02 10:58:14 -0500

    adds three variables to typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,6 +25,9 @@ const typeDefs = `
     declaration: Boolean!
     critique: Boolean
     price: Float
+    canPurchase: Boolean
+    purchasePrice: Float
+    canDownload: Boolean
     selectedMedium: String!
     user: User
     comments: [Comment]
@@ -47,7 +50,7 @@ const typeDefs = `
   type Mutation {
     addUser(userName: String!, bio: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    uploadImage(userId: ID!, image: String, title: String!, description: String!, declaration: Boolean!, critique: Boolean, price: Float!, selectedMedium: String!): ImageDetails!
+    uploadImage(userId: ID!, image: String, title: String!, description: String!, declaration: Boolean!, critique: Boolean, price: Float!, canDownload: Boolean, purchasePrice: Float!, canPurchase: Boolean, selectedMedium: String!): ImageDetails!
     addComment(imageId: ID!, comment: String!): ImageDetails
   }
   `;
