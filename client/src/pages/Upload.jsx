dfe575b586b75f9ac8212d72af110ef63c7dc66f
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-01 22:18:49 -0500

    adds styling to the 'allow critique' checkbox

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -192,43 +192,29 @@ return (
                                 rows={3}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 defaultValue={''}
-                              />
+                                />
                             </div>
                           </div>
+                        </div>
 
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Artwork Price
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="price"
-                  name="price"
-                  rows={1}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={"$" + ''}
-                />
-              </div>
-            </div>
-
-            <div className="relative flex gap-x-3">
-                  <div className="flex h-6 items-center">
-                    <input
-                      id="critique"
-                      name="critique"
-                      type="checkbox"
-                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                  </div>
-                  <div className="text-sm leading-6">
-                    <label htmlFor="comments" className="font-medium text-gray-900">
-                      Allow Critique
-                    </label>
-                    <p className="text-gray-500">Check this box allow critique on this work from other artists</p>
-                  </div>
-                </div>
-          </div>
-        </div>
+                        {/* This is the div for the 'allow critique' checkbox */}
+                        <div className="my-5">
+                          <h2 className="text-3x1 font-semibold leading-6 text-gray-900">Allow Critique</h2>
+                            <div className="relative flex gap-x-3">
+                              <input
+                                id="critique"
+                                name="critique"
+                                type="checkbox"
+                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                              />
+                              <div className="text-sm leading-6">
+                                <label htmlFor="comments" className="font-medium text-gray-900">
+                                  Check this box to allow critique on this work from other artists
+                                </label>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
 
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Sale Options</h2>
@@ -264,6 +250,22 @@ return (
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                     />
                   </div>
+
+                  <div className="col-span-full">
+              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                Artwork Price
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="price"
+                  name="price"
+                  rows={1}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={"$" + ''}
+                />
+              </div>
+            </div>
+
                   <div className="text-sm leading-6">
                     <label htmlFor="candidates" className="font-medium text-gray-900">
                       Physical Artwork
