5a470311a799bf0f73c2580a32fe579c0a6b5633
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-01 21:59:57 -0500

    style artwork description section on upload form

--- client/src/pages/Upload.jsx
+++ client/src/pages/Upload.jsx
@@ -176,29 +176,26 @@ return (
                                   <option value="6">Photography</option>
                                   <option value="7">Other</option>
                                 </select>
-                                
                               </div>
                             </div>
                           </div>
 
+                          {/* This div contains the artwork description input section */}
+                          <div className="col-span-full">
+                            <h2 className="text-3x1 font-semibold leading-6 text-gray-900">
+                              Artwork Description
+                            </h2>
+                            <div className="mt-2">
+                              <textarea
+                                id="description"
+                                name="description"
+                                rows={3}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                defaultValue={''}
+                              />
+                            </div>
+                          </div>
 
-            
-                 
-
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Artwork Description
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="description"
-                  name="description"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-            </div>
             <div className="col-span-full">
               <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
                 Artwork Price
