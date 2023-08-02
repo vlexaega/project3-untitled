4a43459db2b8fb63d778fd26d1bca2c1aba632ad
University <university@AA687692-Mac.local>
2023-08-02 10:44:14 -0500

    final inital front end setup

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -25,3 +25,24 @@ const Message = ({ message }) => (
     <p>{message}</p>
   </section>
 );
+  function Cart() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      setMessage('Order placed! You will receive an email confirmation.');
+    }
+    if (query.get('canceled')) {
+      setMessage(
+        'Order canceled -- continue to shop around and checkout when you\'re ready.'
+      );
+    }
+  }, []);
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay />
+  );
+}
+export default Cart
