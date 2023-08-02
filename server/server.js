32a915fa94ee107b1f9df868591426843da648af
University <university@AA687692-Mac.local>
2023-08-02 10:39:24 -0500

    setup stripe in server.js

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ const startApolloServer = async () => {
         });
     });
 };
-
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${domain}?success=true`,
+      cancel_url: `${domain}?canceled=true`,
+    });
+    res.redirect(303, session.url);
+  });
+  app.listen(3001, () => console.log('Running on port 3001'));
 //call the function to start the server
 startApolloServer();
