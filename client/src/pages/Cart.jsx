610eb3d528d3e566059fe34e057f7be1841346bb
University <university@AA687692-Mac.local>
2023-08-02 10:42:16 -0500

    starter code for front end

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,20 +1,16 @@
 import Navbar from "../components/Navbar";
 import React, { useState, useEffect } from "react";
 
-function Cart(){
-    return(
-        <>
-        <main>
-            <Navbar />
-        <section>
+const ProductDisplay = () => (
+  <section>
     <div className="product">
       <img
         src="https://i.imgur.com/EHyR2nP.png"
         alt="The cover of Stubborn Attachments"
       />
       <div className="description">
-      <h3>Stubborn Attachments</h3>
-      <h5>$20.00</h5>
+        <h3>Stubborn Attachments</h3>
+        <h5>$20.00</h5>
       </div>
     </div>
     <form action="/create-checkout-session" method="POST">
@@ -23,13 +19,9 @@ function Cart(){
       </button>
     </form>
   </section>
-  
-
-        </main>
-        </>
-
-    )
-}
-
-export default Cart 
-
+);
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
