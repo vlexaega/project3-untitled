2c38d733e4b21958f44c65a23aa5ac84496d07de
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-02 22:19:01 -0500

    adds fabric card to medium page

--- client/src/pages/ArtByMedium.jsx
+++ client/src/pages/ArtByMedium.jsx
@@ -45,6 +45,11 @@ const ArtByMedium = () => {
 
   let mixedMediaImg = images.find(image => getMixedMedia(image))
 
+  function getFabric(image) {
+    return image.selectedMedium === "Fabric"
+  }
+
+  let fabricImg = images.find(image => getFabric(image))
 
   return (
     <div>
@@ -231,13 +236,17 @@ const ArtByMedium = () => {
 
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
+            <Link
+                    to={`/workofart/${fabricImg._id}`} key={fabricImg._id}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="p-2 rounded max-h-96 max-w-sm"
+                      src={fabricImg.image}
+                      alt="digital artwork"
+                    />
+                  </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-3xl">
