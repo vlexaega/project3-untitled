fac71fef9d6b27247a72702ac066b15d7b76b9d6
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-02 22:16:11 -0500

    adds mixed media card to medium page

--- client/src/pages/ArtByMedium.jsx
+++ client/src/pages/ArtByMedium.jsx
@@ -39,6 +39,12 @@ const ArtByMedium = () => {
 
   let drawingImg = images.find(image => getDrawing(image))
 
+  function getMixedMedia(image) {
+    return image.selectedMedium === "Mixed Media"
+  }
+
+  let mixedMediaImg = images.find(image => getMixedMedia(image))
+
 
   return (
     <div>
@@ -137,13 +143,17 @@ const ArtByMedium = () => {
 
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
+            <Link
+                    to={`/workofart/${mixedMediaImg._id}`} key={mixedMediaImg._id}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="p-2 rounded max-h-96 max-w-sm items-center"
+                      src={mixedMediaImg.image}
+                      alt="digital artwork"
+                    />
+                  </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-3xl">
