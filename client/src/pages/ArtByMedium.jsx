8dd01dd64dc55ef5f433bfd925199bde5a9c997b
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-02 21:12:01 -0500

    sets the photograpy medium card with image from database

--- client/src/pages/ArtByMedium.jsx
+++ client/src/pages/ArtByMedium.jsx
@@ -231,13 +231,17 @@ const ArtByMedium = () => {
 
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
+            <Link
+                    to={`/workofart/${digitalImg._id}`} key={digitalImg._id}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="p-2 rounded max-h-96 max-w-sm"
+                      src={digitalImg.image}
+                      alt="photography artword"
+                    />
+                  </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-3xl">
