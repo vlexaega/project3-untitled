ad6ac1bebf7931377709aa768fc72f1df6342b75
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-08-02 22:46:14 -0500

    removes unecessary imports from ArtByMedium.jsx file

--- client/src/pages/ArtByMedium.jsx
+++ client/src/pages/ArtByMedium.jsx
@@ -1,9 +1,7 @@
 import Navbar from "../components/Navbar";
-import { Navigate, useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_ALL_IMAGES } from "../utils/queries";
-import React, { useState } from "react";
-import { useEffect } from "react";
 
 const ArtByMedium = () => {
   const { loading, error, data } = useQuery(QUERY_ALL_IMAGES);
